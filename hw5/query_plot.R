49f75330711bebb045b8568b368eaa1c24e25ff5
Jordan Sibley <jordan.c.sibley@gmail.com>
2025-06-06 20:16:32 -0700

    Update plot

--- hw5/query_plot.R
+++ hw5/query_plot.R
@@ -6,11 +6,11 @@ results <- read.csv("index_results.csv")
 # Preview it
 head(results)
 
-ggplot(results, aes(x = label, y = log(avg_time))) +
+ggplot(results, aes(x = log(num_distinct), y = log(avg_time))) +
     geom_point() +
     coord_flip() +
     labs(title = "Average Query Time by Index",
-         x = "Index Label",
+         x = "Log of Number of Distinct Values",
          y = "Log of Average Query Time (s)") +
     theme_minimal()
 
